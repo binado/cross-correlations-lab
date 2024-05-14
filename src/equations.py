c2a96622a6d4132bee1637bb601ed32b52954c1b
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-05-14 15:23:27 -0300

    Use camb to compute distances

--- src/equations.py
+++ src/equations.py
@@ -1,63 +1,60 @@
 import numpy as np
 from scipy.special import erfc
 
-from .utils import lognormal_arg, critical_surface_density, normalize
+from .camb_interface import CAMBInterface
+from .utils import lognormal_arg, critical_surface_density, normalize, G, c
 
-def sfunc(z, zmin, zmax, cosmology, sigma):
+def sfunc(z, zmin, zmax, cambi: CAMBInterface, sigma):
     """
     See Eq. 6.
     """
-    dmin = cosmology.luminosity_distance(zmin).value
-    dmax = cosmology.luminosity_distance(zmax).value
-    d = cosmology.luminosity_distance(z).value
+    dmin = cambi.dl(zmin)
+    dmax = cambi.dl(zmax)
+    d = cambi.dl(z)
     return 0.5 * (erfc(lognormal_arg(dmin, d, sigma)) - erfc(lognormal_arg(dmax, d, sigma)))
 
-def tfunc(z, zmin, zmax, cosmology, sigma):
+def tfunc(z, zmin, zmax, cambi: CAMBInterface, sigma):
     """
     See Eq. 9
     """
-    dmin = cosmology.luminosity_distance(zmin).value
-    dmax = cosmology.luminosity_distance(zmax).value
-    d = cosmology.luminosity_distance(z).value
+    dmin = cambi.dl(zmin)
+    dmax = cambi.dl(zmax)
+    d = cambi.dl(z)
     xmin = lognormal_arg(dmin, d, sigma)
     xmax = lognormal_arg(dmax, d, sigma)
     norm = sigma * np.sqrt(2 * np.pi)
     return (np.exp(-xmax ** 2) - np.exp(-xmin ** 2)) / norm
 
-def ws(z, ngw, si, dc2_over_hz, norm=False):
+def ws(ngw, si, dc2_over_hz):
     """
     See Eq. 8
     Normalization is equivalent to dividing by \bar{n} in the paper
     """
-    res = ngw * dc2_over_hz * si
-    return normalize(res, z) if norm else res
+    return c * ngw * dc2_over_hz * si
 
-def wt(z, ngw, ti, dc2_over_hz, norm=False):
+def wt(ngw, ti, dc2_over_hz):
     """
     See Eq. 8
     Normalization is equivalent to dividing by \bar{n} in the paper
     """
-    res = ngw * dc2_over_hz * ti
-    return normalize(res, z) if norm else res
+    return c * ngw * dc2_over_hz * ti
 
-def wg(z, ng, dc2_over_hz, zmin, zmax, norm=False):
+def wg(z, ng, dc2_over_hz, zmin, zmax):
     """
     See Eq. 15
     """
     cut = (z >= zmin) & (z <= zmax)
-    res = ng * dc2_over_hz * cut
-    return normalize(res, z) if norm else res
+    return c * ng * dc2_over_hz * cut
 
 
 def wkappa(z, zprime, cosmology):
     """
     See Eq. 4
     """
-    if z > zprime:
-        return 0.
-    pcr = cosmology.critical_density(0)
+    domain = z <= zprime
+    pcr = cosmology.critical_density(0).value
     omegam = cosmology.Om0
     num  = omegam * pcr * (1 + zprime) ** 3
-    denom = cosmology.H(zprime) * critical_surface_density(z, zprime, cosmology)
-    return num / denom
+    denom = cosmology.H(zprime).value * critical_surface_density(z, zprime, cosmology)
+    return domain * num / denom
 
