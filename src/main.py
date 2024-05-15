4bbdd3bcae2a9ad49c21be265f8fdcb420e4db5e
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-05-14 21:56:17 -0300

    Adapt main script to code changes

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ import json
 
 import numpy as np
 
-from .angular_power_spectra import CrossPowerSpectra, RedshiftBin
 from .camb_interface import CAMBInterface
+from .cosmology import Cosmology
+from .density import *
+from .kernel import *
+from .tracer import *
 from .parser import parser
+from .power import Power
 from .logger import LoggerConfig
 
 if __name__ == '__main__':
@@ -27,40 +31,65 @@ if __name__ == '__main__':
     logger.info('Running Boltzmann solver')
     camb_interface = CAMBInterface(args.zmin, args.zmax)
     camb_interface.run_solver()
+    cosmology = Cosmology(camb_interface)
 
     # Defining redshift bins
     logger.info('Defining redshift bins')
     nbins = args.nbins
     zbins = np.linspace(args.zmin, args.zmax, nbins + 1)
-    bins = [RedshiftBin(zbins[i], zbins[i + 1]) for i in range(nbins)]
-    zmids = [bin.zmid for bin in bins]
+    bins = [RadialBin(zbins[i], zbins[i + 1]) for i in range(nbins)]
+    zmids = [bin.center for bin in bins]
+
+    # Defining tracers
+    logger.info('Defining tracers')
+    g_density = UniformInVolumeNumberDensity(1e-3, cosmology)
+    gw_density = UniformInVolumeNumberDensity(3e-6, cosmology)
+
+    g_kernels = [ClusteringKernel('galaxy clustering', 'g')]
+    gw_kernels = [
+        ClusteringKernel('gw clustering', 's'),
+        WeakLensingKernel('gw lensing', 't')
+        ]
+
+    g_window_functions = [BoxWindowFunction()]
+    gw_window_functions = [
+        GWClusteringWindowFunction(cosmology, 0.05),
+        GWLensingWindowFunction(cosmology, 0.05)
+        ]
+
+    g_bias = ConstantBias()
+    gw_bias = ConstantBias()
+
+    g_tracer = Tracer(g_density, g_kernels, g_window_functions, g_bias)
+    gw_tracer = Tracer(gw_density, gw_kernels, gw_window_functions, gw_bias)
 
     # Power spectra pre-processing
     logger.info('Power spectra pre-processing')
-    cp = CrossPowerSpectra(z, bins, camb_interface)
+    cp = Power(zbins, cosmology)
 
     l = args.l
-    k = cp.k(l, cp.zz)
-    logger.info('%s', k)
     galbin = args.galbin
-    #print(cp.pm(z, 1, grid=False))
-    cls = np.empty((nbins, len(cp.labels)))
+    cls = np.empty((nbins, g_tracer.nkernels, gw_tracer.nkernels))
     for i in range(nbins):
         logger.info('Computing cls for bins %s, %s', i, galbin)
-        _,  cls[i, :]= cp.compute(l, bins[i], bins[galbin], 1, 1)
+        cls[i, :, :]= cp.cls(l, bins[i], bins[galbin], g_tracer, gw_tracer, z)
 
     if args.save_figure:
         import matplotlib.pyplot as plt
 
         logger.info('Plotting figures')
-        nfig = len(cp.labels)
-        fig, axs = plt.subplots(nfig, 1, sharex=True, figsize=(4, 3 * nfig))
+        fig, axs = plt.subplots(g_tracer.nkernels, gw_tracer.nkernels, squeeze=False)
         binvec = np.arange(nbins)
-        for i, (label, ax) in enumerate(zip(cp.labels, axs)):
-            ax.plot(zmids, cls[:, i])
-            ax.set_ylabel(label)
-            ax.set_xlabel('z')
-            ax.grid()
+        labels = [
+            [f'c{k2.symbol}{k1.symbol}' for k2 in gw_tracer.kernels]
+            for k1 in g_tracer.kernels
+        ]
+        for i in range(g_tracer.nkernels):
+            for j in range(gw_tracer.nkernels):
+                axs[i][j].plot(zmids, cls[:, i, j])
+                axs[i][j].set_xlabel('z')
+                axs[i][j].set_title(labels[i][j])
+                axs[i][j].grid()
         
         fig.tight_layout()
         fig.savefig(f"{outdir}/cls.{args.figure_format}", dpi=400)
