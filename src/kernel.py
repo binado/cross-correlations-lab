c5ed4851ee0e464177fc18cabfbf13a8c7d97524
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-05-24 00:30:07 -0300

    Fix expression for lensing kernel

--- src/kernel.py
+++ src/kernel.py
@@ -24,14 +24,15 @@ class ClusteringKernel(Kernel):
 class WeakLensingKernel(Kernel):
     @classmethod
     def at_z(cls, z, n, cosmology, bias=1):
-        zz, zzp = np.meshgrid(z, z, indexing='ij')
+        zzp, zz = np.meshgrid(z, z, indexing='xy')
         dh = cosmology.hubble_distance
         chi = cosmology.chi(z)
 
         res = 1.5 * cosmology.Om0 * (1. + z) * chi / dh ** 2
-        n_mesh = mesh(n, indexing='ij')
-        chi_mesh = mesh(chi, indexing='ij')
+        n_mesh = np.meshgrid(n, n, indexing='xy')[0]
+        chi_mesh = np.meshgrid(chi, chi, indexing='xy')[0]
 
-        reduced_kernel = n_mesh * cosmology.da12(zzp, zz) / chi_mesh
-        integrated_reduced_kernel = np.trapz(reduced_kernel, z, axis=0)
+        reduced_kernel = n_mesh * cosmology.da12(zz, zzp) / chi_mesh
+        reduced_kernel[zz >= zzp] = 0.
+        integrated_reduced_kernel = np.trapz(reduced_kernel, z, axis=1)
         return res * integrated_reduced_kernel
