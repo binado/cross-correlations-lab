d455e393de1bddb0802ab4f6bc3c8da6386e31c5
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-05-24 18:16:08 -0300

    Allow one window function per tracer

--- src/tracer.py
+++ src/tracer.py
@@ -5,10 +5,9 @@ from .utils import normalize
 class Tracer:
     """Class representing a tracer of matter.
     """
-    def __init__(self, number_density, kernels, window_functions, bias):
+    def __init__(self, number_density, kernels, window_function, bias):
         """Create a new `Tracer` class instance.
 
-        We allow for a different window function for each kernel for flexibility.
         See Eqs.(5), (9) of https://arxiv.org/abs/1603.02356.
 
         Parameters
@@ -17,34 +16,28 @@ class Tracer:
             The number density of the tracer
         kernels : list[kernel.Kernel]
             The different kernels to apply for the tracer (e.g clustering, lensing)
-        window_functions : list[window.WindowFunction]
+        window_function : window.WindowFunction
             The different window functions to apply for the tracer. 
             The length of the list must equal that of `kernels`.
         bias : bias.Bias
             The bias model for the tracer
         """        
         self.density = number_density
-
-        if len(kernels) != len(window_functions):
-            raise ValueError
-
         self.kernels = kernels
-        self.window_functions = window_functions
+        self.window_function = window_function
         self.bias = bias
 
     @property
     def nkernels(self):
         return len(self.kernels)
 
-    def normalized_density(self, zbin, window_function, z):
+    def normalized_density(self, zbin, z):
         """Compute normalized density for given redshift bin and window function.
 
         Parameters
         ----------
         zbin : density.RadialBin
             The redshift bin
-        window_function : window.WindowFunction
-            The window function to consider
         z : array_like
             Array of redshifts
 
@@ -53,7 +46,7 @@ class Tracer:
         array_like
             Normalized density over z
         """        
-        density = self.density.at_z(z) * window_function.at_z(z, zbin.low, zbin.high)
+        density = self.density.at_z(z) * self.window_function.at_z(z, zbin.low, zbin.high)
         return normalize(density, z)
 
     def compute_kernel_functions(self, zbin, cosmology, z):
@@ -76,8 +69,8 @@ class Tracer:
         nz = z.size
         res = np.empty((self.nkernels, nz))
         bias = self.bias.at_z(zbin.center)
-        for i, (kernel, window_function) in enumerate(zip(self.kernels, self.window_functions)):
-            n = self.normalized_density(zbin, window_function, z)
+        n = self.normalized_density(zbin, z)
+        for i, kernel in enumerate(self.kernels):
             res[i, :] = kernel.at_z(z, n, cosmology, bias)
 
         return res
